46f8645eeb259f39a00caa096563f8d7392891e1
Arror <hallo.maqiang@icloud.com>
2023-03-27 12:19:27 +0800

    Remove default

--- Mango/MGAppDelegate.swift
+++ Mango/MGAppDelegate.swift
@@ -4,10 +4,6 @@ import UserNotifications
 final class MGAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        MGNetworkViewModel.setupDefaultLogIfNeeded()
-        MGSniffingViewModel.setupDefaultSettingsIfNeeded()
-        MGLogViewModel.setupDefaultLogIfNeeded()
-        MGRouteViewModel.setupDefaultSettingsIfNeeded()
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert], completionHandler: { _, _ in })
         UNUserNotificationCenter.current().delegate = self
         return true
